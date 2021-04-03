21505b2dc439f0b5007fb4776014a493a317bfdb
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-03 02:24:29 +0200

    fix(animation-fade-in): removing storemap

--- src/components/animation-fade-in.js
+++ src/components/animation-fade-in.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { animated, useSpring } from 'react-spring'
-import { StoreMap } from '../store/store-map'
 
-export const AnimationFadeIn = StoreMap (({ payload }) => {
+export const AnimationFadeIn = ({ payload }) => {
 
     const p = useSpring ({ 'opacity': 1, 'from': { 'opacity': 0 }})
 
     return <animated.span style={p}>{payload}</animated.span>
 
-})
+}
